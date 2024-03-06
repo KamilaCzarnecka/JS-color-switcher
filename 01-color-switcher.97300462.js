6fd9ffe37f6cf11c2dd64445371f54c7c78eef43
KamilaCzarnecka <143633220+KamilaCzarnecka@users.noreply.github.com>
2024-03-06 17:22:37 +0000

    Deploying to gh-pages from @ KamilaCzarnecka/goit-js-hw-09@28a15015b7ee4d3fe6b84b1172e50a42d690ba1d 🚀

---
+++ 01-color-switcher.97300462.js
@@ -0,0 +1,2 @@
+!function(){var t=!0;function e(){t&&(document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),setTimeout(e,1e3))}document.querySelector("[data-start]").addEventListener("click",(function(){this.disabled=!0,document.querySelector("[data-stop]").disabled=!1,t=!0,e()})),document.querySelector("[data-stop]").addEventListener("click",(function(){this.disabled=!0,document.querySelector("[data-start]").disabled=!1,t=!1}))}();
+//# sourceMappingURL=01-color-switcher.97300462.js.map
